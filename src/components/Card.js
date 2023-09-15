bf532daa5600e82ca70f453c6f55d0d177f82058
tajraab <tajrabacevac@gmail.com>
2023-09-15 17:45:37 +0200

    kartica

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./card.css";
 function Card() {
-  return <div>Ovo je nasa Card</div>;
+  return (
+    <div className="card">
+      <h1>Tajra Bacevac</h1>
+      <h1>31.01.2006</h1>
+      <h1>Gimnazija</h1>
+      <h1>Instagram</h1>
+      <h1>Github</h1>
+    </div>
+  );
 }
 export default Card;
