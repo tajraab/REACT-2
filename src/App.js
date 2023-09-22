e8ea46c13bdccb1479e1efe62b0a8583d65cd989
tajraab <tajrabacevac@gmail.com>
2023-09-22 15:51:42 +0200

    domaci

--- src/App.js
+++ src/App.js
@@ -69,20 +69,62 @@ function App() {
   //   </div>
   // );
 
-  const [brojevi, setBroj] = useState([12, 6, 35, 15, 21, 56, 82, 10]);
-  const [noviBroj, setNoviBroj] = useState(undefined);
-  console.log(brojevi);
-  return (
-    <div className="container">
-      <input
-        placeholder="Dodaj broj"
-        value={noviBroj}
-        onChange={(e) => setNoviBroj(e.target.value)}
-      />
-      <button onClick={() => setBroj((prev) => [...prev, noviBroj])}>
-        Dodaj broj
-      </button>
-    </div>
-  );
+  // const [brojevi, setBroj] = useState([12, 6, 35, 15, 21, 56, 82, 10]);
+  // const [noviBroj, setNoviBroj] = useState(undefined);
+  // console.log(brojevi);
+  // return (
+  //   <div className="container">
+  //     <input
+  //       placeholder="Dodaj broj"
+  //       value={noviBroj}
+  //       onChange={(e) => setNoviBroj(e.target.value)}
+  //     />
+  //     <button
+  //       onClick={() => {
+  //         setBroj((prev) => [...prev, noviBroj]);
+  //         setNoviBroj("");
+  //       }}
+  //     >
+  //       Dodaj broj
+  //     </button>
+  //   </div>
+  // );
+
+  /////////////////////////////////////////////////////////////////
+
+  const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("");
+    return (
+      <div className="create">
+        <h2>Add a new blog</h2>
+        <form>
+          <label>Tajra Bacevac:</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>School:</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label>Years:</label>
+          <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+            <option value="mario">mario</option>
+            <option value="yoshi"></option>
+          </select>
+
+          <button>Add Blog</button>
+          <p>{title}</p>
+          <p>{body}</p>
+        </form>
+      </div>
+    );
+  };
 }
 export default App;
