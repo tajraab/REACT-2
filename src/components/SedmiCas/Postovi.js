044c7d146357333b7b5dae516dfba13f7364443e
tajraab <tajrabacevac@gmail.com>
2023-10-15 14:48:51 +0200

    posts

--- src/components/SedmiCas/Postovi.js
+++ src/components/SedmiCas/Postovi.js
@@ -1,4 +1,13 @@
 import React from "react";
 export default function Postovi({ posts }) {
-  return <h1>T</h1>;
+  return (
+    <div className="postt">
+      <h1>
+        <span className="keys">{posts.title}</span>
+      </h1>
+      <h2>
+        <span className="keys">{posts.reactions}</span>
+      </h2>
+    </div>
+  );
 }
